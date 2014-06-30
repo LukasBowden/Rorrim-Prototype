0b3f059d652ecdec19a23eed72b4d33910ec6964
Lukas Platz-Bowden <bowden.lukas@gmail.com>
2014-06-30 20:53:07 +1000

    Level Exit prefab and code added, put the level name into public variable and that will load the level when player collides with it

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoad : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.tag == "Player")
+		{
+			Application.LoadLevel(levelName);
+		}
+	}
+}
