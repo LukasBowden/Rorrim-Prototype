d0fd3cf315b16f24130c3ec56010807fbb89bd1c
Kevin Coppock <K.Coppock1@gmail.com>
2014-07-01 11:31:31 +1000

    Sprites pushed this time. (Hopefully)

---
+++ Assets/Scripts/TeleportingSFXControler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportingSFXControler : MonoBehaviour {
+
+	public AudioClip button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(0))
+		{
+			if (Input.GetMouseButtonDown(1)) 
+			{
+			audio.PlayOneShot(button);
+			}
+		}
+	}
+}
