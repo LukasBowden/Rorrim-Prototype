4f4d698cafdafdd4e4b888af189ae30bf3325f80
Lukas Platz-Bowden <bowden.lukas@gmail.com>
2014-07-02 14:27:50 +1000

    Rotator now works as intended, you can now input limits of both positive and negative values. This will limit rotation between the two values, setting them both to the same number will remove limits.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,18 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : MonoBehaviour
+{
+	public float upperAngleLimit = 0;
+	public float lowerAngleLimit = 0;
 
-	float deltaRotation;
-	float previousRotation;
 	float currentRotation;
 
 	bool clickable = false;
 
-	// Use this for initialization
 	void Start () 
 	{
-		//setup the size of the graphic and hitbox
 		transform.localScale = new Vector3(transform.parent.transform.localScale.y/transform.parent.transform.localScale.x, 1, 1);//new Vector3(transform.parent.transform.localScale.y, transform.parent.transform.localScale.y, 0);
 	}
 
@@ -22,41 +21,41 @@ public class Rotator : MonoBehaviour {
 			clickable = true;
 	}
 
-	// Update is called once per frame
 	void Update () 
 	{
-		if(clickable)
+		if(clickable)		
 		{
 			if (Input.GetMouseButtonDown (0) && Input.GetKey(KeyCode.LeftShift))
 			{
-				deltaRotation = 0f;
-				previousRotation = angleBetweenPoints( transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+				currentRotation = transform.parent.transform.eulerAngles.z;
 			}
 			else if(Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftShift))
 			{
-				currentRotation = angleBetweenPoints( transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition) );
-				deltaRotation = Mathf.DeltaAngle( currentRotation, previousRotation );
-				previousRotation = currentRotation;
-				transform.parent.transform.Rotate( Vector3.back, deltaRotation );
+				if(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x > 0)
+				{
+					currentRotation += Input.GetAxis("Mouse Y") * 15;
+				}
+				if(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y > 0)
+				{
+					currentRotation -= Input.GetAxis("Mouse X") * 15;
+				}
+				if(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x < 0)
+				{
+					currentRotation -= Input.GetAxis("Mouse Y") * 15;
+				}
+				if(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y < 0)
+				{
+					currentRotation += Input.GetAxis("Mouse X") * 15;
+				}
+				if(lowerAngleLimit != upperAngleLimit)
+					currentRotation = Mathf.Clamp (currentRotation, lowerAngleLimit, upperAngleLimit);
+			
+				transform.parent.transform.eulerAngles = new Vector3(0, 0, currentRotation);
+			}
+			if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.LeftShift))
+			{
+				clickable = false;
 			}
 		}
-		if (Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.LeftShift))
-		{
-			clickable = false;
-		}
-	}
-
-	float angleBetweenPoints( Vector2 position1, Vector2 position2 )
-	{
-		Vector2 fromLine = position2 - position1;
-		Vector2 toLine = new Vector2( 1, 0 );
-		
-		float angle = Vector2.Angle( fromLine, toLine );
-		Vector3 cross = Vector3.Cross( fromLine, toLine );
-
-		if( cross.z > 0 )
-			angle = 360f - angle;
-		
-		return angle;
 	}
 }
